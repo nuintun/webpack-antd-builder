c88de5ef424abe8cf517d8a1c016551723c22a1a
nuintun <nuintun@qq.com>
2018-06-18 10:00:12 +0800

    :art: Rewrite dev server use koa

--- webpack/webpack.config.server.js
+++ webpack/webpack.config.server.js
@@ -8,60 +8,70 @@
 
 'use strict';
 
+const koa = require('koa');
 const webpack = require('webpack');
-const getPort = require('./lib/port');
+const pkg = require('../package.json');
 const loaders = require('./lib/loaders');
 const globEntry = require('./lib/entry');
+const koaWebpack = require('koa-webpack');
+const koaCompress = require('koa-compress');
 const getLocalExternalIP = require('./lib/ip');
 const configure = require('./webpack.config.base');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const WebpackEntryManifestPlugin = require('webpack-entry-manifest-plugin');
 const { entry, entryBasePath, publicPath, outputPath, sourceMapExclude } = require('./configure');
 
+const app = new koa();
 const mode = 'development';
-const ip = getLocalExternalIP();
-const port = getPort(process.argv);
-const origin = `http://${ip}:${port}`;
-const devServerPublicPath = origin + publicPath;
-const watcher = globEntry(entry, entryBasePath, [`webpack-dev-server/client?${origin}`, 'webpack/hot/only-dev-server']);
 
 process.env.NODE_ENV = mode;
 process.env.BABEL_ENV = mode;
 
-configure.mode = mode;
-configure.stats = 'none';
-configure.devtool = 'none';
-configure.entry = watcher.entries();
-configure.output = Object.assign(configure.output, {
-  filename: 'js/[name].js',
-  chunkFilename: 'js/[name].js',
-  publicPath: devServerPublicPath
-});
-configure.plugins = [
-  watcher,
-  ...configure.plugins,
-  new webpack.EnvironmentPlugin({ DEBUG: true, NODE_ENV: mode }),
-  new MiniCssExtractPlugin({ filename: 'css/[name].css' }),
-  new webpack.SourceMapDevToolPlugin({ exclude: sourceMapExclude }),
-  new WebpackEntryManifestPlugin({ map: (file, chunk) => `${file}?v=${chunk.hash}` })
-];
-configure.module.rules = loaders(true);
-configure.devServer = {
-  port,
-  hot: true,
-  quiet: true,
-  inline: true,
-  overlay: true,
-  compress: true,
-  public: origin,
-  host: '0.0.0.0',
-  clientLogLevel: 'none',
-  watchContentBase: true,
-  contentBase: outputPath,
-  publicPath: devServerPublicPath,
-  historyApiFallback: { disableDotRule: true },
-  watchOptions: { ignored: 'node_modules/**/*' },
-  headers: { 'Access-Control-Allow-Origin': '*' }
-};
+app.use(koaCompress());
 
-module.exports = configure;
+const server = app.listen(() => {
+  const ip = getLocalExternalIP();
+  const port = server.address().port;
+  const devServerHost = `//${ip}:${port}`;
+  const devServerPublicPath = devServerHost + publicPath;
+  const watcher = globEntry(entry, entryBasePath, ['webpack-hot-client/client']);
+
+  configure.mode = mode;
+  configure.stats = 'none';
+  configure.devtool = 'none';
+  configure.entry = watcher.entries();
+  configure.output = Object.assign(configure.output, {
+    filename: 'js/[name].js',
+    chunkFilename: 'js/[name].js',
+    publicPath: devServerPublicPath
+  });
+  configure.plugins = [
+    watcher,
+    ...configure.plugins,
+    new webpack.EnvironmentPlugin({ DEBUG: true, NODE_ENV: mode }),
+    new MiniCssExtractPlugin({ filename: 'css/[name].css' }),
+    new webpack.SourceMapDevToolPlugin({ exclude: sourceMapExclude }),
+    new WebpackEntryManifestPlugin({ map: (file, chunk) => `${file}?v=${chunk.hash}` })
+  ];
+  configure.module.rules = loaders(true);
+
+  const compiler = webpack(configure);
+
+  compiler.name = pkg.name;
+
+  koaWebpack({
+    compiler,
+    hotClient: {
+      logLevel: 'silent'
+    },
+    devMiddleware: {
+      logLevel: 'silent',
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      }
+    }
+  }).then(middleware => {
+    app.use(middleware);
+  });
+});
