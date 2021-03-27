ea1854afb960e662e58d18a8d41de4b427600123
nuintun <nuintun@qq.com>
2021-03-27 22:05:36 +0800

    :tada: Update files

--- Assets/js/hooks/createSharedState.ts
+++ Assets/js/hooks/createSharedState.ts
@@ -3,50 +3,35 @@ import React, { useEffect, useState } from 'react';
 import { isFunction } from '~js/utils/utils';
 import useIsomorphicLayoutEffect from './useIsomorphicLayoutEffect';
 
-interface Store<S> {
-  state: S;
-  setState: Setter<S>;
-  setters: Setter<S>[];
-}
+type SetState<S> = React.Dispatch<React.SetStateAction<S>>;
 
-type Setter<S> = React.Dispatch<React.SetStateAction<S>>;
-
-export default function createSharedState<S>(): () => [state: S | undefined, setState: Setter<S | undefined>];
-export default function createSharedState<S>(initialState: S | (() => S)): () => [state: S, setState: Setter<S>];
+export default function createSharedState<S>(): () => [state: S | undefined, setState: SetState<S | undefined>];
+export default function createSharedState<S>(initialState: S | (() => S)): () => [state: S, setState: SetState<S>];
 export default function createSharedState<S>(
   initialState?: S | (() => S)
-): () => [state: S | undefined, setState: Setter<S | undefined>] {
-  const state = isFunction(initialState) ? initialState() : (initialState as S);
+): () => [state: S | undefined, setState: SetState<S | undefined>] {
+  let dispatches = new Set<SetState<S>>();
+  let sharedState = isFunction(initialState) ? initialState() : (initialState as S);
 
-  const store: Store<S> = {
-    state,
-    setters: [],
-    setState(state: React.SetStateAction<S>): void {
-      const nextState = isFunction(state) ? state(store.state) : state;
+  const setSharedState = (value: React.SetStateAction<S>): void => {
+    sharedState = isFunction(value) ? value(sharedState) : value;
 
-      store.state = nextState;
-
-      store.setters.forEach(setter => setter(nextState));
-    }
+    dispatches.forEach(dispatch => dispatch(sharedState));
   };
 
   return () => {
-    const [state, setState] = useState(store.state);
+    const [state, setState] = useState(sharedState);
 
     useIsomorphicLayoutEffect(() => {
-      const { setters } = store;
-
-      if (!setters.includes(setState)) {
-        setters.push(setState);
-      }
+      dispatches.add(setState);
     }, []);
 
     useEffect(() => {
       return () => {
-        store.setters = store.setters.filter(setter => setter !== setState);
+        dispatches.delete(setState);
       };
     }, []);
 
-    return [state, store.setState];
+    return [state, setSharedState];
   };
 }
