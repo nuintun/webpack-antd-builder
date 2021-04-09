d05217a7757454a7ffd9212126e09867d8824cfc
nuintun <nuintun@qq.com>
2021-04-09 10:26:24 +0800

    :tada: Update files

--- .babelrc.js
+++ .babelrc.js
@@ -9,14 +9,15 @@
 
 const browsers = require('./package.json').browserslist;
 
+const targets = { browsers };
 const corejs = { version: '^3.0.0', proposals: true };
 const development = process.env.BABEL_ENV !== 'production';
 const imports = { style: true, libraryName: 'antd', libraryDirectory: 'es' };
 
 module.exports = {
   presets: [
-    ['@babel/preset-env', { bugfixes: true, corejs, useBuiltIns: 'usage', targets: { browsers } }],
-    ['@babel/preset-react', { runtime: 'automatic', useBuiltIns: true, development }],
+    ['@babel/preset-env', { bugfixes: true, corejs, useBuiltIns: 'usage', targets }],
+    ['@babel/preset-react', { runtime: 'automatic', development }],
     ['@babel/preset-typescript', { allowDeclareFields: true }]
   ],
   plugins: [
