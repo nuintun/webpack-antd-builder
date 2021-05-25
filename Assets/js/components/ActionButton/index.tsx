8398938341eb377a4344940211dfc5b393836c85
nuintun <nuintun@qq.com>
2021-05-25 16:30:59 +0800

    :tada: Update files

--- Assets/js/components/ActionButton/index.tsx
+++ Assets/js/components/ActionButton/index.tsx
@@ -5,19 +5,20 @@ import { Options, RequestError } from '~js/utils/request';
 import { QuestionCircleOutlined } from '@ant-design/icons';
 import usePersistCallback from '~js/hooks/usePersistCallback';
 import { Button, ButtonProps, message, Popconfirm, PopconfirmProps } from 'antd';
+import { isFunction } from '~js/utils/utils';
 
-export interface ActionButtonProps<T> extends Omit<ButtonProps, 'loading' | 'onClick' | 'onError'> {
+export interface ActionButtonProps<T>
+  extends Pick<Options, 'method' | 'notify'>,
+    Omit<ButtonProps, 'loading' | 'onClick' | 'onError'> {
   delay?: number;
   action: string;
-  method?: string;
-  notify?: boolean;
-  body?: Options['body'];
   onComplete?: () => void;
-  query?: Options['query'];
   onSuccess?: (response: T) => void;
   confirmIcon?: PopconfirmProps['icon'];
   confirmTitle?: PopconfirmProps['title'];
   onError?: (error: RequestError) => void;
+  body?: (() => Options['body']) | Options['body'];
+  query?: (() => Options['query']) | Options['query'];
   requestInit?: Omit<Options, 'query' | 'body' | 'method' | 'notify'>;
   confirmInit?: Omit<PopconfirmProps, 'icon' | 'title' | 'disabled' | 'onConfirm'>;
 }
@@ -25,8 +26,6 @@ export interface ActionButtonProps<T> extends Omit<ButtonProps, 'loading' | 'onC
 const DEFAULT_CONFIRM_ICON = <QuestionCircleOutlined style={{ color: '#f00' }} />;
 
 export default memo(function ActionButton<T>({
-  body,
-  query,
   delay,
   action,
   notify,
@@ -39,12 +38,17 @@ export default memo(function ActionButton<T>({
   confirmInit,
   confirmTitle,
   method = 'PUT',
+  body: initBody,
+  query: initQuery,
   confirmIcon = DEFAULT_CONFIRM_ICON,
   ...restProps
 }: ActionButtonProps<T>): React.ReactElement {
   const [loading, fetch] = useRequest(delay);
 
   const onAction = usePersistCallback(async () => {
+    const body = isFunction(initBody) ? initBody() : initBody;
+    const query = isFunction(initQuery) ? initQuery() : initQuery;
+
     try {
       const response = await fetch<T>(action, { ...requestInit, method, body, query, notify });
 
