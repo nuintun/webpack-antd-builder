4ebfc7fc6d0e34d642cafaf6585f51d8059d4442
nuintun <nuintun@qq.com>
2021-09-17 19:42:26 +0800

    ✨ Update compose utils

--- Assets/js/utils/compose.ts
+++ Assets/js/utils/compose.ts
@@ -9,24 +9,36 @@ export type Compose<C> = (context: C) => Promise<void>;
 export type Middleware<C> = (context: C, next: Next) => Promise<void> | void;
 
 /**
+ * @function dispatch
+ * @param middlewares 中间件数组
+ * @param index 要执行的中间件索引
+ * @param current 正在执行的中间件信息
+ * @param context 执行上下文
+ */
+async function dispatch<C>(middlewares: Middleware<C>[], index: number, current: { index: number }, context: C): Promise<void> {
+  if (index <= current.index) {
+    throw new Error('next() called multiple times');
+  }
+
+  current.index = index;
+
+  if (index < middlewares.length) {
+    const middleware = middlewares[index];
+
+    return middleware(context, () => dispatch(middlewares, index + 1, current, context));
+  }
+}
+
+/**
  * @function compose
- * @description 生成中间件洋葱圈模型
+ * @description 获取以洋葱圈方式执行中间件的方法
  * @param middlewares 中间件数组
  */
 export default function compose<C>(middlewares: Middleware<C>[]): Compose<C> {
-  const dispatch = async (index: number, current: { index: number }, context: C): Promise<void> => {
-    if (index <= current.index) {
-      throw new Error('next() called multiple times');
-    }
-
-    current.index = index;
-
-    if (index < middlewares.length) {
-      const middleware = middlewares[index];
-
-      return middleware(context, () => dispatch(index + 1, current, context));
-    }
-  };
-
-  return context => dispatch(0, { index: -1 }, context);
+  /**
+   * @function dispatch
+   * @description 以洋葱圈方式执行中间件
+   * @param context 执行上下文
+   */
+  return context => dispatch(middlewares, 0, { index: -1 }, context);
 }
