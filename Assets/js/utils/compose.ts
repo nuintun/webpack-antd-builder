e2d11b7ff7cef43fd3455540f81871b32bc1d202
nuintun <nuintun@qq.com>
2021-09-27 15:00:51 +0800

    ✨ Update compose

--- Assets/js/utils/compose.ts
+++ Assets/js/utils/compose.ts
@@ -2,43 +2,66 @@
  * @module compose
  */
 
+interface CallStack {
+  index: number;
+  done: boolean;
+}
+
 export type Next = () => Promise<void>;
 
-export type Compose<C> = (context: C) => Promise<void>;
-
 export type Middleware<C> = (context: C, next: Next) => Promise<void> | void;
+
+export type ComposedMiddleware<C> = (context: C, next?: Next) => Promise<void>;
 
 /**
  * @function dispatch
  * @param middlewares 中间件数组
  * @param index 要执行的中间件索引
- * @param current 正在执行的中间件信息
+ * @param stack 调用栈信息
  * @param context 执行上下文
+ * @param [next] 下一个中间件
  */
-async function dispatch<C>(middlewares: Middleware<C>[], index: number, current: { index: number }, context: C): Promise<void> {
-  if (index <= current.index) {
+async function dispatch<C>(
+  middlewares: Middleware<C>[],
+  index: number,
+  stack: CallStack,
+  context: C,
+  next?: Next
+): Promise<void> {
+  if (index <= stack.index) {
     throw new Error('next() called multiple times');
   }
 
-  current.index = index;
+  stack.index = index;
 
-  if (index < middlewares.length) {
-    const middleware = middlewares[index];
+  const { length } = middlewares;
 
-    return middleware(context, () => dispatch(middlewares, index + 1, current, context));
+  if (!stack.done && index <= length) {
+    stack.done = index === length;
+
+    if (!stack.done) {
+      const middleware = middlewares[index];
+
+      return middleware(context, () => dispatch(middlewares, index + 1, stack, context, next));
+    } else if (next) {
+      return next();
+    }
   }
 }
 
 /**
  * @function compose
- * @description 获取以洋葱圈方式执行中间件的方法
+ * @description 生成融合中间件
  * @param middlewares 中间件数组
  */
-export default function compose<C>(middlewares: Middleware<C>[]): Compose<C> {
+export default function compose<C>(middlewares: Middleware<C>[]): ComposedMiddleware<C> {
   /**
-   * @function dispatch
-   * @description 以洋葱圈方式执行中间件
+   * @function middleware
+   * @description 融合中间件
    * @param context 执行上下文
+   * @param [next] 下一个中间件
    */
-  return context => dispatch(middlewares, 0, { index: -1 }, context);
+  return async (context, next) => {
+    await dispatch<C>(middlewares, 0, { index: -1, done: false }, context, next);
+  };
 }
