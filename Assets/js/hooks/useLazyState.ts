404f203fca7e43328ef60b1080f06d2c4a2f9d6a
nuintun <nuintun@qq.com>
2021-10-29 21:27:34 +0800

    ✨ Update hooks

--- Assets/js/hooks/useLazyState.ts
+++ Assets/js/hooks/useLazyState.ts
@@ -35,19 +35,24 @@ export default function useLazyState<S = undefined>(
   const timerRef = useRef<Timeout>();
   const delayRef = usePersistRef(delay);
   const [state, setState] = useSafeState(initialState);
+  const prevStateRef = usePersistRef<S | undefined>(state);
 
   const setLazyState = useCallback((value: React.SetStateAction<S | undefined>, immediate?: boolean): void => {
     clearTimeout(timerRef.current);
 
-    const delay = delayRef.current;
-    const nextState = isFunction(value) ? value(state) : value;
+    const prevState = prevStateRef.current;
+    const nextState = isFunction(value) ? value(prevState) : value;
 
-    if (immediate || delay <= 0) {
-      setState(nextState);
-    } else {
-      timerRef.current = setTimeout(() => {
+    if (nextState !== prevState) {
+      const delay = delayRef.current;
+
+      if (immediate || delay <= 0) {
         setState(nextState);
-      }, delay);
+      } else {
+        timerRef.current = setTimeout(() => {
+          setState(nextState);
+        }, delay);
+      }
     }
   }, []);
 
