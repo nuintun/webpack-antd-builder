9e9f64ae4dd0721c4abe6e07a25c439f4d4b5a82
nuintun <nuintun@qq.com>
2022-02-22 14:51:23 +0800

    ✨ Add linked list

--- Assets/js/utils/LinkedList.ts
+++ Assets/js/utils/LinkedList.ts
@@ -6,18 +6,64 @@ export interface Node<T> {
   next: Node<T> | null;
 }
 
-type Callback<T> = (value: T, index: number, self: LinkedList<T>) => boolean;
+type Callback<T> = (currentValue: T, index: number, currentLinkedList: LinkedList<T>) => boolean;
+
+function normalizeIndex(size: number, fromIndex: number = 0): number {
+  return fromIndex < 0 ? Math.max(0, size + fromIndex) : fromIndex;
+}
 
 export default class LinkedList<T> {
-  private size: number = 0;
-  private head: Node<T> | null = null;
-  private tail: Node<T> | null = null;
+  protected size: number = 0;
+  protected head: Node<T> | null = null;
+  protected tail: Node<T> | null = null;
 
-  constructor(iterable: Iterable<T> = []) {
+  public constructor(iterable: Iterable<T> = []) {
     this.push(...iterable);
   }
 
-  unshift(...values: T[]) {
+  protected search(
+    callback: Callback<T>,
+    reverse?: boolean,
+    context?: any
+  ): [node: Node<T>, index: number] | [node: undefined, index: -1] {
+    const { size } = this;
+
+    if (size > 0) {
+      const callbackBound = callback.bind(context);
+
+      if (reverse) {
+        let index = size - 1;
+        let current = this.tail;
+
+        while (!isNull(current)) {
+          if (callbackBound(current.value, index, this)) {
+            return [current, index];
+          } else {
+            current = current.prev;
+          }
+
+          index--;
+        }
+      } else {
+        let index = 0;
+        let current = this.head;
+
+        while (!isNull(current)) {
+          if (callbackBound(current.value, index, this)) {
+            return [current, index];
+          } else {
+            current = current.next;
+          }
+
+          index++;
+        }
+      }
+    }
+
+    return [, -1];
+  }
+
+  public unshift(...values: T[]) {
     for (const value of values) {
       const { head } = this;
       const node: Node<T> = {
@@ -38,7 +84,7 @@ export default class LinkedList<T> {
     }
   }
 
-  shift(): T | undefined {
+  public shift(): T | undefined {
     const { head } = this;
 
     if (!isNull(head)) {
@@ -56,7 +102,7 @@ export default class LinkedList<T> {
     }
   }
 
-  push(...values: T[]) {
+  public push(...values: T[]) {
     for (const value of values) {
       const { tail } = this;
       const node: Node<T> = {
@@ -77,7 +123,7 @@ export default class LinkedList<T> {
     }
   }
 
-  pop(): T | undefined {
+  public pop(): T | undefined {
     const { tail } = this;
 
     if (!isNull(tail)) {
@@ -95,44 +141,123 @@ export default class LinkedList<T> {
     }
   }
 
-  splice(index: number, count: number, ...values: T[]): T | undefined {
-    console.log(index, count, ...values);
+  public splice(index: number, deleteCount: number, ...values: T[]): T | undefined {
+    console.log(index, deleteCount, ...values);
 
     return undefined;
   }
 
-  private search(callback: Callback<T>, context: any = this): [value: Node<T>, index: number] | [value: undefined, index: -1] {
+  public find(callback: Callback<T>, context?: any): T | undefined {
+    const [node] = this.search(callback, false, context);
+
+    return node?.value;
+  }
+
+  public findIndex(callback: Callback<T>, context?: any): number {
+    const [, index] = this.search(callback, false, context);
+
+    return index;
+  }
+
+  public at(index: number): T | undefined {
+    const { size } = this;
+    const position = normalizeIndex(size, index);
+
+    if (position + 1 <= size) {
+      const [node] = this.search((_value, index) => {
+        return index === position;
+      }, position > size / 2);
+
+      return node?.value;
+    }
+  }
+
+  public includes(valueToFind: T, fromIndex?: number): boolean {
+    const { size } = this;
+    const position = normalizeIndex(size, fromIndex);
+
+    if (position + 1 <= size) {
+      const [, index] = this.search((value, index) => {
+        return value === valueToFind && index >= position;
+      }, position > size / 2);
+
+      return index >= 0;
+    }
+
+    return false;
+  }
+
+  public indexOf(valueToFind: T, fromIndex?: number): number {
+    const { size } = this;
+    const position = normalizeIndex(size, fromIndex);
+
+    if (position + 1 <= size) {
+      const [, index] = this.search((value, index) => {
+        return value === valueToFind && index >= position;
+      });
+
+      return index;
+    }
+
+    return -1;
+  }
+
+  public lastIndexOf(valueToFind: T, fromIndex?: number): number {
+    const { size } = this;
+    const position = normalizeIndex(size, fromIndex);
+
+    if (position + 1 <= size) {
+      const [, index] = this.search((value, index) => {
+        return value === valueToFind && index >= position;
+      }, true);
+
+      return index;
+    }
+
+    return -1;
+  }
+
+  public forEach(callback: Callback<T>, context?: any): void {
     let index = 0;
     let current = this.head;
 
     const callbackBound = callback.bind(context);
 
     while (!isNull(current)) {
-      if (callbackBound(current.value, index, this)) {
-        return [current, index];
-      } else {
-        current = current.next;
-      }
+      callbackBound(current.value, index, this);
+
+      current = current.next;
 
       index++;
     }
-
-    return [, -1];
   }
 
-  find(callback: Callback<T>, context: any = this): T | undefined {
-    const [node] = this.search(callback, context);
+  public values(): Iterator<T> {
+    let current = this.head;
 
-    return node?.value;
+    return {
+      next: () => {
+        if (isNull(current)) {
+          return {
+            done: true,
+            value: undefined
+          };
+        }
+
+        const { value } = current;
+
+        current = current.next;
+
+        return { done: false, value };
+      }
+    };
   }
 
-  findIndex(callback: Callback<T>, context: any = this): number {
-    const [, index] = this.search(callback, context);
-
-    return index;
-  }
-
-  get length(): number {
+  public get length(): number {
     return this.size;
+  }
+
+  public [Symbol.iterator]() {
+    return this.values();
   }
 }
