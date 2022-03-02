be2302b7443fe597b3e71b2c7f6e6fb3657d8b70
nuintun <nuintun@qq.com>
2022-03-02 16:33:34 +0800

    ✨ Add bfs tree

--- Assets/js/utils/tree.ts
+++ Assets/js/utils/tree.ts
@@ -2,11 +2,72 @@
  * @module tree
  */
 
+interface QueueNode<T> {
+  value: T;
+  prev: QueueNode<T> | null;
+  next: QueueNode<T> | null;
+}
+
 type Resolve<T> = (node: T) => T[] | void;
 
 type IteratorValue<T> = [node: T, parent: T | undefined];
 
 type Waiting<T> = [iterator: Iterator<T, undefined>, parent?: T];
+
+/**
+ * @class FIFO 队列
+ */
+class Queue<T> {
+  private head: QueueNode<T> | null = null;
+  private tail: QueueNode<T> | null = null;
+
+  /**
+   * @method enqueue
+   * @description 入列
+   * @param value 要入列的值
+   */
+  enqueue(value: T): void {
+    const self = this;
+    const { tail } = self;
+
+    const node: QueueNode<T> = {
+      value,
+      next: null,
+      prev: tail
+    };
+
+    if (tail) {
+      tail.next = node;
+    } else {
+      self.head = node;
+    }
+
+    self.tail = node;
+  }
+
+  /**
+   * @method dequeue
+   * @description 出列
+   */
+  dequeue(): T | undefined {
+    const self = this;
+    const { head } = self;
+
+    if (head) {
+      const { next } = head;
+
+      if (next) {
+        next.prev = null;
+      } else {
+        self.tail = next;
+      }
+
+      self.head = next;
+
+      return head.value;
+    }
+  }
+}
 
 /**
  * @class DFSTree
@@ -48,6 +109,64 @@ export class DFSTree<T> {
 
         if (children && children.length > 0) {
           waiting.push(current);
+
+          current = [children.values(), node];
+        }
+
+        yield [node, parent];
+      }
+    }
+  }
+
+  /**
+   * @private iterator
+   * @description 内置 for...of 迭代器
+   */
+  [Symbol.iterator]() {
+    return this.values();
+  }
+}
+
+/**
+ * @class BFSTree
+ * @description 广度遍历树
+ */
+export class BFSTree<T> {
+  private root: T[];
+
+  /**
+   * @constructor
+   * @description 广度遍历树
+   * @param tree 要广度遍历的树
+   * @param resolve 子节点获取方法
+   */
+  constructor(tree: T, private resolve: Resolve<T>) {
+    this.root = [tree];
+  }
+
+  /**
+   * @method values
+   * @description 节点迭代器
+   */
+  *values(): Iterator<IteratorValue<T>, void> {
+    const { root, resolve } = this;
+    const waiting = new Queue<Waiting<T>>();
+
+    let current: Waiting<T> | undefined = [root.values()];
+
+    while (current) {
+      const [iterator] = current;
+      const item = iterator.next();
+
+      if (item.done) {
+        current = waiting.dequeue();
+      } else {
+        const node = item.value;
+        const [, parent] = current;
+        const children = resolve(node);
+
+        if (children && children.length > 0) {
+          waiting.enqueue(current);
 
           current = [children.values(), node];
         }
