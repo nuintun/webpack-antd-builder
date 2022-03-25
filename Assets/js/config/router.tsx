05ff2f817a276ffb2377c478d44d052be6b8cb8b
nuintun <nuintun@qq.com>
2022-03-25 11:14:17 +0800

    ✨ Update code

--- Assets/js/config/router.tsx
+++ Assets/js/config/router.tsx
@@ -11,6 +11,9 @@ import HomeIcon from '/images/menus/home.svg';
 import AnalysisIcon from '/images/menus/analysis.svg';
 import UserAnalysisIcon from '/images/menus/user-analysis.svg';
 import RechargeAnalysisIcon from '/images/menus/recharge-analysis.svg';
+import SystemIcon from '/images/menus/system.svg';
+import AccountSystemIcon from '/images/menus/account-system.svg';
+import LogsSystemIcon from '/images/menus/logs-system.svg';
 
 const Forbidden = lazy(() => import('/js/pages/403'));
 const ServerError = lazy(() => import('/js/pages/500'));
@@ -78,6 +81,31 @@ export const router: Route<{ id?: number }>[] = [
             meta: {
               name: '充值分析',
               icon: <Icon component={RechargeAnalysisIcon} />
+            }
+          }
+        ]
+      },
+      {
+        path: 'system',
+        meta: {
+          name: '系统管理',
+          icon: <Icon component={SystemIcon} />
+        },
+        children: [
+          {
+            path: 'account',
+            element: <p style={{ padding: 8 }}>帐号管理</p>,
+            meta: {
+              name: '帐号管理',
+              icon: <Icon component={AccountSystemIcon} />
+            }
+          },
+          {
+            path: 'logs',
+            element: <p style={{ padding: 8 }}>安全日志</p>,
+            meta: {
+              name: '安全日志',
+              icon: <Icon component={LogsSystemIcon} />
             }
           }
         ]
