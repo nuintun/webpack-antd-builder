79933f3294cf6851f04b6ecaf88d57a67cd832a1
nuintun <nuintun@qq.com>
2022-03-30 10:09:46 +0800

    ✨ Update hooks

--- Assets/js/hooks/createSharedReducer.ts
+++ Assets/js/hooks/createSharedReducer.ts
@@ -1,51 +1,30 @@
 /**
- * @module createSharedReducer
+ * @module createSharedState
  */
 
-import React, { useCallback, useEffect, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import { isFunction } from '/js/utils/utils';
 
 /**
- * @function createSharedReducer
- * @description [hook] 生成共享 Reducer
- * @param reducer 状态生成器
+ * @function createSharedState
+ * @description [hook] 生成共享状态
  */
-export default function createSharedReducer<S>(
-  reducer: React.ReducerWithoutAction<S | undefined>
-): () => [state: S | undefined, dispatch: React.DispatchWithoutAction];
+export default function createSharedState<S = undefined>(): () => [
+  state: S | undefined,
+  setState: React.Dispatch<React.SetStateAction<S | undefined>>
+];
 /**
- * @function createSharedReducer
- * @description [hook] 生成共享 Reducer
- * @param reducer 状态生成器
- */
-export default function createSharedReducer<S, A>(
-  reducer: React.Reducer<S | undefined, A>
-): () => [state: S | undefined, dispatch: React.Dispatch<A>];
-/**
- * @function createSharedReducer
- * @description [hook] 生成共享 Reducer
- * @param reducer 状态生成器
+ * @function createSharedState
+ * @description [hook] 生成共享状态
  * @param initialState 初始状态
  */
-export default function createSharedReducer<S>(
-  reducer: React.ReducerWithoutAction<S>,
+export default function createSharedState<S>(
   initialState: S | (() => S)
-): () => [state: S, dispatch: React.DispatchWithoutAction];
-/**
- * @function createSharedReducer
- * @description [hook] 生成共享 Reducer
- * @param reducer 状态生成器
- * @param initialState 初始状态
- */
-export default function createSharedReducer<S, A>(
-  reducer: React.Reducer<S, A>,
-  initialState: S | (() => S)
-): () => [state: S, dispatch: React.Dispatch<A>];
-export default function createSharedReducer<S, A>(
-  reducer: React.Reducer<S | undefined, A>,
+): () => [state: S, setState: React.Dispatch<React.SetStateAction<S>>];
+export default function createSharedState<S = undefined>(
   initialState?: S | (() => S)
-): () => [state: S | undefined, dispatch: React.Dispatch<A>] {
+): () => [state: S | undefined, setState: React.Dispatch<React.SetStateAction<S | undefined>>] {
   let initialized = false;
   let sharedState: S | undefined;
 
@@ -59,8 +38,8 @@ export default function createSharedReducer<S, A>(
     return sharedState;
   };
 
-  const dispatchActions = (action: A): void => {
-    sharedState = reducer(sharedState, action);
+  const dispatchActions = (value: React.SetStateAction<S | undefined>): void => {
+    sharedState = isFunction(value) ? value(sharedState) : value;
 
     for (const dispatch of dispatches) {
       dispatch(sharedState);
@@ -70,27 +49,20 @@ export default function createSharedReducer<S, A>(
   const dispatches = new Set<React.Dispatch<React.SetStateAction<S | undefined>>>();
 
   return () => {
-    const initializedRef = useRef(false);
     const [state, setState] = useState(getInitialState);
 
-    if (!initializedRef.current) {
-      dispatches.add(setState);
-
-      initializedRef.current = true;
-    }
-
-    const dispatch = useCallback<React.Dispatch<A>>(action => {
-      initializedRef.current && dispatchActions(action);
+    const setSharedState = useCallback((value: React.SetStateAction<S | undefined>): void => {
+      dispatchActions(value);
     }, []);
 
     useEffect(() => {
+      dispatches.add(setState);
+
       return () => {
         dispatches.delete(setState);
-
-        initializedRef.current = false;
       };
     }, []);
 
-    return [state, dispatch];
+    return [state, setSharedState];
   };
 }
