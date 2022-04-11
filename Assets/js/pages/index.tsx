94e691d3eba0e537afce168a431bda503b6bff7b
nuintun <nuintun@qq.com>
2022-04-11 16:59:50 +0800

    ✨ Update code

--- Assets/js/pages/index.tsx
+++ Assets/js/pages/index.tsx
@@ -20,7 +20,7 @@ if (__DEV__) {
 
   on('ok', ({ builtAt }) => {
     console.info(
-      `%c⭕%c[HMR]: App is up to date at ${new Date(builtAt).toLocaleString()}.`,
+      `%c⭕%c[HMR]: App is up to date at ${new Date(builtAt).toLocaleString()}`,
       'color: #f92f60; font-weight: normal;',
       'color: #1890ff; font-weight: bold;'
     );
