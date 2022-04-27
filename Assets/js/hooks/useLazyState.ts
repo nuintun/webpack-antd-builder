9fc71d8f548f94613c133f87c71b7887cb417a23
nuintun <nuintun@qq.com>
2022-04-27 16:45:36 +0800

    ✨ Update hooks

--- Assets/js/hooks/useLazyState.ts
+++ Assets/js/hooks/useLazyState.ts
@@ -2,7 +2,7 @@
  * @module useLazyState
  */
 
-import React, { useCallback, useRef } from 'react';
+import React, { useCallback, useEffect, useRef } from 'react';
 
 import useSafeState from './useSafeState';
 import usePersistRef from './usePersistRef';
