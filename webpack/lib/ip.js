e1f1b8e6ec6636102b998c3ade744b855aae88bd
nuintun <nuintun@qq.com>
2022-05-30 14:49:24 +0800

    chore: Update ip resolver

--- webpack/lib/ip.js
+++ webpack/lib/ip.js
@@ -7,24 +7,26 @@
 
 import os from 'os';
 
+const IPV4_RE = /^\d{1,3}(?:\.\d{1,3}){3}$/;
+
 /**
  * @function resolveIp
- * @param {string} family
+ * @param {boolean} ipv4
  * @returns {Promise<string>}
  */
-export default async (family = 'IPv4') => {
+export default async (ipv4 = true) => {
   const networkInterfaces = os.networkInterfaces();
   const interfaces = Object.keys(networkInterfaces);
 
   for (const face of interfaces) {
     const networkInterface = networkInterfaces[face];
 
-    for (const network of networkInterface) {
-      if (!network.internal && network.family === family) {
-        return network.address;
+    for (const { address, internal } of networkInterface) {
+      if (!internal && ipv4 && IPV4_RE.test(address)) {
+        return address;
       }
     }
   }
 
-  return '127.0.0.1';
+  return ipv6 ? '::1' : '127.0.0.1';
 };
