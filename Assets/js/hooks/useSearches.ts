334381d1434e2aea5d55deff5cc8748f536797cc
nuintun <nuintun@qq.com>
2022-06-23 17:19:39 +0800

    fix: Fixed useSearches

--- Assets/js/hooks/useSearches.ts
+++ Assets/js/hooks/useSearches.ts
@@ -25,7 +25,9 @@ export default function useSearches<T extends Search[]>(
       searchRef.current = searchRef.current.map((search, index) => {
         const nextValue = value[index];
 
-        return nextValue === false ? search : nextValue;
+        if (nextValue === false) return nextValue;
+
+        return nextValue ?? search;
       }) as T;
     }
 
