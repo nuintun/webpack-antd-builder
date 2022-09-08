978c80db67a64b3751735baf92535f917890ed39
nuintun <nuintun@qq.com>
2022-09-08 15:34:53 +0800

    feat: Update hooks

--- Assets/js/hooks/useStableCallback.ts
+++ Assets/js/hooks/useStableCallback.ts
@@ -6,12 +6,16 @@ import { useCallback } from 'react';
 
 import useSyncRef from './useSyncRef';
 
+export interface Callback {
+  (this: any, ...args: any[]): any;
+}
+
 /**
  * @function useStableCallback
  * @description [hook] 将指定函数包装成稳定函数
  * @param callback 回调函数
  */
-export default function useStableCallback<C extends (...args: any[]) => any>(callback: C): C {
+export default function useStableCallback<C extends Callback>(callback: C): C {
   const callbackRef = useSyncRef(callback);
 
   return useCallback(((...args) => callbackRef.current(...args)) as C, []);
