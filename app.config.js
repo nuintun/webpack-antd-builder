47972017997dd8725f82bc9284e0b5ba55bba583
nuintun <nuintun@qq.com>
2022-12-12 16:43:49 +0800

    chore: Update app config

--- app.config.js
+++ app.config.js
@@ -21,6 +21,6 @@ export default {
   entryHTML: path.resolve('wwwroot/app.html'),
   favicon: path.resolve('wwwroot/favicon.ico'),
   entry: [path.resolve('Assets/js/pages/index.tsx')],
-  alias: { '/js': js, '/css': css, '/mods': mods, '/images': images },
+  alias: { '/js': js, '/css': css, '/images': images },
   meta: { 'theme-color': '#1668dc', viewport: 'width=device-width,initial-scale=1.0' }
 };
