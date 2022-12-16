afedd2cee078e4fcda1f6327bb3c078081b70410
nuintun <nuintun@qq.com>
2022-12-16 15:53:50 +0800

    feat: Adjust directory structure

--- app.config.js
+++ app.config.js
@@ -7,19 +7,19 @@
 
 import path from 'path';
 
-const js = path.resolve('Assets/js');
-const css = path.resolve('Assets/css');
-const images = path.resolve('Assets/images');
+const js = path.resolve('app/js');
+const css = path.resolve('app/css');
+const images = path.resolve('app/images');
 
 export default {
   name: 'Antd',
   lang: 'zh-CN',
   publicPath: '/public/',
-  context: path.resolve('Assets'),
+  context: path.resolve('app'),
   outputPath: path.resolve('wwwroot/public'),
   entryHTML: path.resolve('wwwroot/app.html'),
   favicon: path.resolve('wwwroot/favicon.ico'),
-  entry: [path.resolve('Assets/js/pages/index.tsx')],
+  entry: [path.resolve('app/js/pages/index.tsx')],
   alias: { '/js': js, '/css': css, '/images': images },
   meta: { 'theme-color': '#1668dc', viewport: 'width=device-width,initial-scale=1.0' }
 };
