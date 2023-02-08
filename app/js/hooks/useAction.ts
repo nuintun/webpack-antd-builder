0d60701ac1a3eeab11a25bd058c18573f3f0aeea
nuintun <nuintun@qq.com>
2023-02-08 10:40:05 +0800

    feat: Update hooks

--- app/js/hooks/useAction.ts
+++ app/js/hooks/useAction.ts
@@ -9,27 +9,23 @@ import useRequest, { RequestOptions } from './useRequest';
 
 export interface Options<R> extends RequestOptions<R> {
   delay?: number;
-  action: string;
 }
 
-export default function useAction<R>(
-  action: string,
-  requestInit: RequestOptions<R>,
-  delay?: number
-): [loading: boolean, onAction: () => void] {
+export default function useAction<R>(action: string, options: Options<R>): [loading: boolean, onAction: () => void] {
+  const { delay } = options;
   const actionRef = useLatestRef(action);
-  const requestInitRef = useLatestRef(requestInit);
+  const optionsRef = useLatestRef(options);
   const [loading, request] = useRequest({ delay }, false);
 
   const onAction = useCallback(() => {
-    const { current: requestInit } = requestInitRef;
-    const { method = 'PUT', body: initBody, query: initQuery } = requestInit;
+    const { current: options } = optionsRef;
+    const { method = 'PUT', body: initBody, query: initQuery } = options;
 
     const isBlobBody = initBody instanceof Blob;
     const query = isFunction(initQuery) ? initQuery() : initQuery;
     const body = !isBlobBody && isFunction(initBody) ? initBody() : initBody;
 
-    request<R>(actionRef.current, { ...requestInit, body, query, method });
+    request<R>(actionRef.current, { ...options, body, query, method });
   }, []);
 
   return [loading, onAction];
