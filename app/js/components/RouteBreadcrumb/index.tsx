29b0aa1291f88b42219bb006abf3b68b0d5b2108
nuintun <nuintun@qq.com>
2023-03-06 14:52:15 +0800

    feat: Update Breadcrum

--- app/js/components/RouteBreadcrumb/index.tsx
+++ app/js/components/RouteBreadcrumb/index.tsx
@@ -6,21 +6,11 @@ import React, { memo, useMemo } from 'react';
 
 import classNames from 'classnames';
 import Link from '/js/components/Link';
+import { IRoute } from '/js/utils/router';
 import useStyle, { prefixUI } from './style';
 import FlexIcon from '/js/components/FlexIcon';
 import { useMatches } from 'react-nest-router';
-import { Icon, IRoute } from '/js/utils/router';
 import { Breadcrumb, BreadcrumbProps } from 'antd';
-
-const { Item } = Breadcrumb;
-
-interface BreadcrumbItem {
-  key: string;
-  icon?: Icon;
-  name: string;
-  href?: string;
-  active: boolean;
-}
 
 type BreadcrumbPicked = 'style' | 'className' | 'separator';
 
@@ -28,7 +18,11 @@ export interface RouteBreadcrumbProps extends Pick<BreadcrumbProps, BreadcrumbPi
   icon?: boolean;
 }
 
-function getBreadcrumbs(matches: IRoute[]): BreadcrumbItem[] {
+interface BreadcrumbItem {
+  title: React.ReactNode;
+}
+
+function getBreadcrumbItems(matches: IRoute[], showIcon: boolean): BreadcrumbItem[] {
   const { length } = matches;
   const breadcrumbs: BreadcrumbItem[] = [];
 
@@ -39,16 +33,28 @@ function getBreadcrumbs(matches: IRoute[]): BreadcrumbItem[] {
 
     if (name) {
       const { children } = match;
-      const { key, icon, link } = meta;
+      const { icon, link } = meta;
+      const active = i + 1 >= length;
+      const isLink = !active && children?.some(route => route.index);
 
-      if (i + 1 < length) {
-        if (children?.some(route => route.index)) {
-          breadcrumbs.push({ key, name, icon, href: link.href, active: false });
-        } else {
-          breadcrumbs.push({ key, name, icon, active: false });
-        }
+      if (isLink) {
+        breadcrumbs.push({
+          title: (
+            <Link href={link.href} target={link.target} className={`${prefixUI}-link`}>
+              {showIcon && <FlexIcon icon={icon} className={`${prefixUI}-icon`} />}
+              <span>{name}</span>
+            </Link>
+          )
+        });
       } else {
-        breadcrumbs.push({ key, name, icon, href: link.href, active: true });
+        breadcrumbs.push({
+          title: (
+            <span className={classNames(`${prefixUI}-item`, { active })}>
+              {showIcon && <FlexIcon icon={icon} className={`${prefixUI}-icon`} />}
+              <span>{name}</span>
+            </span>
+          )
+        });
       }
     }
   }
@@ -63,25 +69,7 @@ export default memo(function RouteBreadcrumb({
 }: RouteBreadcrumbProps): React.ReactElement {
   const { render } = useStyle();
   const matches = useMatches() as IRoute[];
-  const breadcrumbs = useMemo(() => getBreadcrumbs(matches), [matches]);
+  const items = useMemo(() => getBreadcrumbItems(matches, showIcon), [matches, showIcon]);
 
-  return render(
-    <Breadcrumb style={style} className={classNames('ui-component', prefixUI, className)}>
-      {breadcrumbs.map(({ key, name, icon, href, active }) => (
-        <Item key={key}>
-          {href && !active ? (
-            <Link href={href} className={`${prefixUI}-link`}>
-              {showIcon && <FlexIcon icon={icon} className={`${prefixUI}-icon`} />}
-              <span>{name}</span>
-            </Link>
-          ) : (
-            <span className={classNames(`${prefixUI}-item`, { active })}>
-              {showIcon && <FlexIcon icon={icon} className={`${prefixUI}-icon`} />}
-              <span>{name}</span>
-            </span>
-          )}
-        </Item>
-      ))}
-    </Breadcrumb>
-  );
+  return render(<Breadcrumb items={items} style={style} className={classNames('ui-component', prefixUI, className)} />);
 });
