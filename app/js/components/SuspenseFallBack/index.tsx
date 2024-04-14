f72d6392967084463d1c86a472a8ecab26de66cc
nuintun <nuintun@qq.com>
2024-04-14 17:13:03 +0800

    feat: Bump deps

--- app/js/components/SuspenseFallBack/index.tsx
+++ app/js/components/SuspenseFallBack/index.tsx
@@ -4,7 +4,7 @@ import { Spin, SpinProps } from 'antd';
 
 export interface SuspenseFallBackProps extends Pick<SpinProps, 'delay'>, Pick<React.CSSProperties, 'width' | 'height'> {}
 
-export default memo(function SuspenseFallBack({ delay = 120, width, height = 360 }: SuspenseFallBackProps): React.ReactElement {
+export default memo(function SuspenseFallBack({ delay = 128, width, height = 360 }: SuspenseFallBackProps): React.ReactElement {
   return (
     <Spin delay={delay}>
       <div style={{ width, height }} />
