1ad9e55faecf006867865ab987ed420c0a57580e
nuintun <nuintun@qq.com>
2024-04-16 11:37:52 +0800

    feat: Bump deps

--- app/js/components/QRCode/index.tsx
+++ app/js/components/QRCode/index.tsx
@@ -7,20 +7,30 @@ import { memo, useMemo } from 'react';
 import { Image, ImageProps } from 'antd';
 import { Alphanumeric, Byte, Encoder, EncoderOptions, Hanzi, Kanji, Numeric } from '@nuintun/qrcode';
 
-export type Segment = Alphanumeric | Byte | Hanzi | Kanji | Numeric;
+type HEX = `#${string}`;
 
-export interface QRCodeProps extends EncoderOptions, Omit<ImageProps, 'src'> {
-  quietZone?: number;
-  moduleSize?: number;
-  background?: string;
-  foreground?: string;
-  segments: Segment | Segment[];
-}
+type Segment = Alphanumeric | Byte | Hanzi | Kanji | Numeric;
 
-function hex2rgb(hex: string): [R: number, G: number, B: number] {
+const SHORT_HEX_RE = /^#([a-f\d])([a-f\d])([a-f\d])([a-f\d])?$/i;
+
+function hex2rgb(hex: HEX): [R: number, G: number, B: number] {
+  hex = hex.replace(SHORT_HEX_RE, (_match, r, g, b, a) => {
+    a = a ?? '';
+
+    return `#${r + r + g + g + b + b + a + a}`;
+  }) as HEX;
+
   const value = parseInt(hex.slice(1, 7), 16);
 
   return [(value >> 16) & 0xff, (value >> 8) & 0xff, value & 0xff];
+}
+
+export interface QRCodeProps extends EncoderOptions, Omit<ImageProps, 'src'> {
+  background?: HEX;
+  foreground?: HEX;
+  quietZone?: number;
+  moduleSize?: number;
+  segments: Segment | Segment[];
 }
 
 export default memo(function QRCode({
@@ -31,8 +41,8 @@ export default memo(function QRCode({
   segments,
   quietZone,
   moduleSize,
-  background = '#ffffff',
-  foreground = '#000000',
+  background = '#fff',
+  foreground = '#000',
   ...props
 }: QRCodeProps) {
   const src = useMemo(() => {
