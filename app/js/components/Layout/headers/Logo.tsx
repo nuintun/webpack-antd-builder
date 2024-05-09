b99bf1afc3cf6ee9e5156cd89ff0abf4c094ac53
nuintun <nuintun@qq.com>
2024-05-09 10:59:20 +0800

    feat: Update code

--- app/js/components/Layout/headers/Logo.tsx
+++ app/js/components/Layout/headers/Logo.tsx
@@ -3,12 +3,12 @@
  */
 
 import React, { memo } from 'react';
+import classNames from 'classnames';
 import Link from '/js/components/Link';
 import useStyles, { prefixUI } from './style/logo';
 import { RenderHeaderProps } from '/js/components/FlexMenu';
 
 import logo from '/images/logo.svg?url';
-import classNames from 'classnames';
 
 export default memo(function LogoHeader({ collapsed }: RenderHeaderProps): React.ReactElement {
   const [scope, render] = useStyles();
