2f85d59efbfc2af7b8d516138fa998f58ece73ef
nuintun <nuintun@qq.com>
2024-06-02 23:10:02 +0800

    feat: Update code

---
+++ app/js/hooks/useAction.tsx
@@ -0,0 +1,80 @@
+/**
+ * @module useAction
+ */
+
+import useLatestRef from './useLatestRef';
+import { isObject } from '/js/utils/utils';
+import { Popconfirm, PopconfirmProps } from 'antd';
+import { useCallback, useRef, useState } from 'react';
+import { QuestionCircleOutlined } from '@ant-design/icons';
+import useSubmit, { Options as UseSubmitOptions, Values } from '/js/hooks/useSubmit';
+
+export interface Options<V extends Values, R> extends UseSubmitOptions<V, R> {
+  delay?: number;
+  disabled?: boolean;
+  confirm?: string | Omit<PopconfirmProps, 'open' | 'trigger' | 'disabled' | 'onConfirm' | 'onOpenChange'>;
+}
+
+export default function useAction<V extends Values, R>(
+  action: string,
+  options: Options<V, R> = {}
+): [loading: boolean, onAction: (values: V) => void, render: (children: React.ReactElement) => React.ReactElement] {
+  const [open, setOpen] = useState(false);
+  const valuesRef = useRef<V | null>(null);
+  const optionsRef = useLatestRef(options);
+  const [loading, onSubmit] = useSubmit(action);
+
+  const onConfirm = useCallback(() => {
+    if (optionsRef.current.confirm) {
+      setOpen(false);
+    }
+
+    onSubmit(valuesRef.current);
+  }, []);
+
+  const onAction = useCallback((values: V) => {
+    const { current: options } = optionsRef;
+
+    if (!options.disabled) {
+      if (options.confirm) {
+        valuesRef.current = values;
+
+        setOpen(true);
+      } else {
+        onSubmit(values);
+      }
+    }
+  }, []);
+
+  const onOpenChange = useCallback((open: boolean) => {
+    setOpen(open);
+  }, []);
+
+  const render = (children: React.ReactElement): React.ReactElement => {
+    const { confirm } = options;
+
+    if (confirm) {
+      const { disabled } = options;
+      const icon = <QuestionCircleOutlined style={{ color: '#f00' }} />;
+      const props: PopconfirmProps = isObject(confirm) ? confirm : { title: '警告', description: confirm };
+
+      return (
+        <Popconfirm
+          icon={icon}
+          {...props}
+          open={open}
+          trigger={[]}
+          disabled={disabled}
+          onConfirm={onConfirm}
+          onOpenChange={onOpenChange}
+        >
+          {children}
+        </Popconfirm>
+      );
+    }
+
+    return children;
+  };
+
+  return [loading, onAction, render];
+}
--- app/js/pages/Analysis/User.tsx
+++ app/js/pages/Analysis/User.tsx
@@ -1,6 +1,29 @@
-import { memo } from 'react';
 import Paper from '/js/components/Paper';
+import { memo, useCallback } from 'react';
+import useAction from '/js/hooks/useAction';
+import { Button, ButtonProps, GetProp } from 'antd';
 
 export default memo(function Page() {
-  return <Paper>用户分析</Paper>;
+  const [loading, onAction, render] = useAction('/api/analysis/user');
+
+  const onClick = useCallback<GetProp<ButtonProps, 'onClick'>>(e => {
+    e.stopPropagation();
+
+    onAction(null);
+  }, []);
+
+  console.log('render', loading);
+
+  return (
+    <Paper>
+      <p>用户分析</p>
+      <div>
+        {render(
+          <Button type="primary" loading={loading} onClick={onClick}>
+            useAction
+          </Button>
+        )}
+      </div>
+    </Paper>
+  );
 });
